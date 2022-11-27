88498d8cda27ba55b71c6b3250fceb2ccda3b213
Ikechukwu Agbarakwe <iaweepingsaint@gmail.com>
2022-11-27 22:26:41 +0100

    Navbar and Styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import ReactLogo from "../images/react-icon-small.png"
 
 export default function Navbar() {
-  return <h1>Navbar component</h1>;
+  return (
+    <nav>
+      <img src={ReactLogo} alt="react small icon" className="nav--icon" />
+      <h3 className="nav--logo_text">ReactFacts</h3>
+      <h4 className="nav--title">React Course - Project 1</h4>
+    </nav>
+  );
 }
